f4f45981f11fae2c7a291ad45be46e3eb279c3a0
maksym-serverless <maksym@serverless.direct>
2023-07-19 12:58:10 +0300

    setRoles, setPermissions and setType fix

--- api/auth/AuthContextProvider.tsx
+++ api/auth/AuthContextProvider.tsx
@@ -119,44 +119,44 @@ const AuthContextProvider = (props: AuthContextProviderProps) => {
     onSuccess: (data) => {
       setProfile(data)
       setFirstName(data['first_name'])
-      // setPermissions(data.permissions)
-      // setRoles(data.roles)
+      setPermissions(data.permissions)
+      setRoles(data.roles)
       setUserType('admin'.toLowerCase())
-      setRoles(['sdp', 'ldb', 'soi', 'msag'])
-      setPermissions([
-        'Auth@CreateUser',
-        'Auth@ListUsers',
-        'Auth@UpdateUser',
-        'Auth@UpdateUserEnabled',
-        'Auth@UpdateUserPassword',
-        'Auth@UpdateUserRolesPermissions',
-        'Ldb@CreateAdrProvider',
-        'Ldb@CreateRecord',
-        'Ldb@DeleteAdrProvider',
-        'Ldb@DeleteRecord',
-        'Ldb@ListAdrProviders',
-        'Ldb@ListRecords',
-        'Ldb@UpdateAdrProvider',
-        'Ldb@UpdateRecord',
-        'Msag@CreateRecord',
-        'Msag@DeleteRecord',
-        // 'Msag@ListImports',
-        'Msag@ListRecords',
-        // 'Msag@ListRecordsHistory',
-        'Msag@UpdateRecord',
-        'Msag@UploadFile',
-        'Sdp@CreateSdp',
-        'Sdp@ListSdps',
-        'Sdp@UpdateSdp',
-        'Sdp@UpdateSdpUsers',
-        'Soi@CreateRecord',
-        'Soi@DeleteRecord',
-        // 'Soi@ListImports',
-        'Soi@ListRecords',
-        'Soi@ListRecordsHistory',
-        'Soi@UpdateRecord',
-        'Soi@UploadFile',
-      ])
+      // setRoles(['sdp', 'ldb', 'soi', 'msag'])
+      // setPermissions([
+      //   'Auth@CreateUser',
+      //   'Auth@ListUsers',
+      //   'Auth@UpdateUser',
+      //   'Auth@UpdateUserEnabled',
+      //   'Auth@UpdateUserPassword',
+      //   'Auth@UpdateUserRolesPermissions',
+      //   'Ldb@CreateAdrProvider',
+      //   'Ldb@CreateRecord',
+      //   'Ldb@DeleteAdrProvider',
+      //   'Ldb@DeleteRecord',
+      //   'Ldb@ListAdrProviders',
+      //   'Ldb@ListRecords',
+      //   'Ldb@UpdateAdrProvider',
+      //   'Ldb@UpdateRecord',
+      //   'Msag@CreateRecord',
+      //   'Msag@DeleteRecord',
+      //   // 'Msag@ListImports',
+      //   'Msag@ListRecords',
+      //   // 'Msag@ListRecordsHistory',
+      //   'Msag@UpdateRecord',
+      //   'Msag@UploadFile',
+      //   'Sdp@CreateSdp',
+      //   'Sdp@ListSdps',
+      //   'Sdp@UpdateSdp',
+      //   'Sdp@UpdateSdpUsers',
+      //   'Soi@CreateRecord',
+      //   'Soi@DeleteRecord',
+      //   // 'Soi@ListImports',
+      //   'Soi@ListRecords',
+      //   'Soi@ListRecordsHistory',
+      //   'Soi@UpdateRecord',
+      //   'Soi@UploadFile',
+      // ])
     },
   })
 
